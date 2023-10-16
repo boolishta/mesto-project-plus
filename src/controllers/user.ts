6af705630660cde78e32057b94ee21c566492d2a
bator <batr.fly@mail.ru>
2023-10-16 10:41:48 +0200

    add token

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { failFind, sendError } from '../errors/index';
 import UserModel from '../models/user';
 
@@ -7,7 +8,12 @@ export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const user = await UserModel
       .findUserByCredentials(email, password);
-    return user;
+    const token = jwt.sign(
+      { _id: user._id },
+      'some-secret-key',
+      { expiresIn: '7d' },
+    );
+    return res.send({ token });
   } catch (error) {
     return sendError(error, res);
   }
